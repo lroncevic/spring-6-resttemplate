be27497b2e6901ae1f6b6309e9f13c034b94fcef
lroncevic <roncevicluka@outlook.com>
2023-04-03 22:19:57 +0200

    Added mockito & getBeerById test

--- src/test/java/com/lukaroncevic/spring6resttemplate/client/BeerClientMockTest.java
+++ src/test/java/com/lukaroncevic/spring6resttemplate/client/BeerClientMockTest.java
@@ -2,39 +2,78 @@ package com.lukaroncevic.spring6resttemplate.client;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lukaroncevic.spring6resttemplate.config.RestTemplateBuilderConfig;
 import com.lukaroncevic.spring6resttemplate.model.BeerDTO;
 import com.lukaroncevic.spring6resttemplate.model.BeerDTOPageImpl;
 import com.lukaroncevic.spring6resttemplate.model.BeerStyle;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.web.client.MockServerRestTemplateCustomizer;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.UUID;
 
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
-@RestClientTest(BeerClientImpl.class)
+@RestClientTest
+@Import(RestTemplateBuilderConfig.class)
 public class BeerClientMockTest {
 
     static final String URL = "http://localhost:8080";
-
-    @Autowired
+    
     BeerClient beerClient;
 
-    @Autowired
     MockRestServiceServer server;
 
     @Autowired
+    RestTemplateBuilder restTemplateBuilderConfigured;
+
+    @Autowired
     ObjectMapper objectMapper;
+
+    @Mock
+    RestTemplateBuilder mockRestTemplateBuilder = new RestTemplateBuilder(new MockServerRestTemplateCustomizer());
+
+    @BeforeEach
+    void setUp() {
+        RestTemplate restTemplate = restTemplateBuilderConfigured.build();
+        server = MockRestServiceServer.bindTo(restTemplate).build();
+        when(mockRestTemplateBuilder.build()).thenReturn(restTemplate);
+        beerClient = new BeerClientImpl(mockRestTemplateBuilder);
+    }
+
+    @Test
+    void getBeerById() throws JsonProcessingException {
+
+        BeerDTO newDto = getBeerDto();
+
+        String response = objectMapper.writeValueAsString(newDto);
+
+        server.expect(method(HttpMethod.GET))
+                .andExpect(requestToUriTemplate(URL + BeerClientImpl.GET_BEER_BY_ID_PATH,
+                        newDto.getId()))
+                .andRespond(withSuccess(response, MediaType.APPLICATION_JSON));
+
+        BeerDTO responseDTO = beerClient.getBeerById(newDto.getId());
+        assertThat(responseDTO.getId()).isEqualTo(newDto.getId());
+
+    }
 
     @Test
     void listBeers() throws JsonProcessingException {
